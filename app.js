33a7c2625cb353298daebd0947f83a2fc011c76d
Alan Margolies <amargolies88@gmail.com>
2020-02-15 18:44:02 -0500

    passed first test!

---
+++ app.js
@@ -0,0 +1,16 @@
+const inquirer = require('inquirer');
+
+
+
+
+
+
+
+// Inquirer Prompt:
+inquirer
+  .prompt([
+    /* Pass your questions in here */
+  ])
+  .then(answers => {
+    // Use user feedback for... whatever!!
+  });
