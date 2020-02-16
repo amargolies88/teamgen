efc2bba0c341e820e7f93246927f157d08ddb5dd
Alan Margolies <amargolies88@gmail.com>
2020-02-15 21:21:57 -0500

    added empty template files and began inquirer prompt

--- app.js
+++ app.js
@@ -1,10 +1,60 @@
 const inquirer = require('inquirer');
+const Employee = require('./lib/Employee');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const Manager = require('./lib/Manager');
+
+// Data Variables
+
+// Team Array
+let team = [];
 
 // Inquirer Prompt:
-inquirer
-  .prompt([
-    /* Pass your questions in here */
-  ])
-  .then(answers => {
-    // Use user feedback for... whatever!!
-  });
+let asking = true;
+let managerExists = false;
+
+function beginQuestions() {
+    inquirer
+        .prompt([
+            {
+                type: "confirm",
+                name: "continueEntering",
+                message: "Add a team member?"
+            },
+        ])
+        .then(answers => {
+            if (!answers.continueEntering) {
+                // finished();
+                asking = false;
+            } else {
+                inquirer
+                    .prompt([
+                        {
+                            type: "list",
+                            name: "memberRole",
+                            message: "Select role",
+                            choices: ["Manager", "Engineer", "Intern"],
+                            validate: (input, answers) => (input != "Manager" || !managerExists) ? true : "This team already has a manager."
+                        }
+                    ])
+                    .then(answers => {
+                        if (answers.memberRole.validate) {
+                            console.log("yay");
+                            console.log(answers.memberRole.validate);
+                            beginQuestions();
+                        } else {
+                            console.log("hello");
+                            console.log(answers.memberRole.validate);
+                            beginQuestions();
+                        }
+                    });
+            }
+        });
+}
+
+beginQuestions();
+
+
+function finished() {
+    console.log("finished adding members")
+}
