e0346be84ee1ef5c7a7e0ce44325becf91919b7a
Alan Margolies <amargolies88@gmail.com>
2020-02-17 08:57:48 -0500

    CCL functions properly

--- app.js
+++ app.js
@@ -13,13 +13,15 @@ let team = [];
 let asking = true;
 let managerExists = false;
 
-function beginQuestions() {
+let fresh = {};
+
+function beginQuestions(firstQuestion) {
     inquirer
         .prompt([
             {
                 type: "confirm",
                 name: "continueEntering",
-                message: "Add a team member?"
+                message: firstQuestion
             },
         ])
         .then(answers => {
@@ -34,25 +36,96 @@ function beginQuestions() {
                             name: "memberRole",
                             message: "Select role",
                             choices: ["Manager", "Engineer", "Intern"],
-                            validate: (input, answers) => (input != "Manager" || !managerExists) ? true : "This team already has a manager."
                         }
+
                     ])
                     .then(answers => {
-                        if (answers.memberRole.validate) {
-                            console.log("yay");
-                            console.log(answers.memberRole.validate);
-                            beginQuestions();
+                        console.log(`member role: ${answers.memberRole}`);
+                        console.log(`manager exists? ${managerExists}`);
+                        if (answers.memberRole != "Manager" || !managerExists) {
+                            fresh.role = answers.memberRole;
+                            if (answers.memberRole === "Manager") managerExists = true;
+                            inquirer
+                                .prompt([
+                                    {
+                                        type: "input",
+                                        name: "memberName",
+                                        message: "Enter name",
+                                        validate: function (value) {
+                                            if (value != '') return true;
+                                            else return "Name must not be blank!"
+                                        }
+                                    },
+                                    {
+                                        type: "input",
+                                        name: "memberId",
+                                        message: "Enter ID Number",
+                                        // This validate taken directly from https://github.com/SBoudrias/Inquirer.js/blob/master/packages/inquirer/examples/pizza.js line:46
+                                        validate: function (value) {
+                                            return (value == parseInt(value)) ? true : "Please enter a valid ID";
+                                        }
+                                    },
+                                    {
+                                        type: "input",
+                                        name: "memberEmail",
+                                        message: "Enter email",
+                                        validate: function (value) {
+                                            return (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(value) ? true : "Enter a valid email address!");
+                                        }
+                                    }
+                                ]).then(answers => {
+                                    fresh.name = answers.memberName;
+                                    fresh.id = answers.memberId;
+                                    fresh.email = answers.memberEmail;
+                                    let uniqueQuestion;
+                                    switch (fresh.role) {
+                                        case "Engineer":
+                                            uniqueQuestion = {
+                                                type: "input",
+                                                name: "uniqueInfo",
+                                                message: "Enter engineer's Github",
+                                                filter: value => `https://github.com/${value}`
+                                            };
+                                            break;
+                                        case "Intern":
+                                            uniqueQuestion = {
+                                                type: "input",
+                                                name: "uniqueInfo",
+                                                message: "Enter intern's school",
+                                            };
+                                            break;
+                                        case "Manager":
+                                            uniqueQuestion = {
+                                                type: "input",
+                                                name: "uniqueInfo",
+                                                message: "Enter manager's office number",
+                                                validate: function (value) {
+                                                    return (value == parseInt(value)) ? true : "Please enter a valid ID";
+                                                }
+                                            };
+                                    }
+
+                                    inquirer
+                                        .prompt(uniqueQuestion)
+                                        .then(answers => {
+                                            fresh.unique = answers.uniqueInfo;
+                                            console.log('New member:');
+                                            console.log(fresh);
+                                            team.push(fresh);
+                                            beginQuestions("Add a team member?");
+                                        });
+
+                                });
                         } else {
-                            console.log("hello");
-                            console.log(answers.memberRole.validate);
-                            beginQuestions();
+                            beginQuestions("Error: This team already has a manager! Would you like to continue adding members?");
                         }
                     });
             }
         });
 }
 
-beginQuestions();
+
+beginQuestions("Add a team member?");
 
 
 function finished() {
